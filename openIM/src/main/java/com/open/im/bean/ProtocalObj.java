05b37bad43649d7694279238a7e1d1f7e523d5fd
Zhlee <1281861976@qq.com>
2016-03-23 17:38:50 +0800

    添加消息状态控件后，聊天详情布局有点儿问题

--- openIM/src/main/java/com/open/im/bean/ProtocalObj.java
+++ openIM/src/main/java/com/open/im/bean/ProtocalObj.java
@@ -1,24 +1,22 @@
 package com.open.im.bean;
 
-import java.io.Serializable;
-
 import com.google.gson.Gson;
-import com.open.im.utils.MyLog;
-import com.thoughtworks.xstream.XStream;
+
+import java.io.Serializable;
 
 public class ProtocalObj  implements Serializable{
 
-	public String toXML() {
-		XStream x = new XStream();
-		x.alias(this.getClass().getSimpleName(), this.getClass());
-		return x.toXML(this);
-	}
-
-	public Object fromXML(String xml) {
-		XStream x = new XStream();
-		x.alias(this.getClass().getSimpleName(), this.getClass());
-		return x.fromXML(xml);
-	}
+//	public String toXML() {
+//		XStream x = new XStream();
+//		x.alias(this.getClass().getSimpleName(), this.getClass());
+//		return x.toXML(this);
+//	}
+//
+//	public Object fromXML(String xml) {
+//		XStream x = new XStream();
+//		x.alias(this.getClass().getSimpleName(), this.getClass());
+//		return x.fromXML(xml);
+//	}
 	public String toJson(){
 		Gson gson = new Gson();
 		return gson.toJson(this);
