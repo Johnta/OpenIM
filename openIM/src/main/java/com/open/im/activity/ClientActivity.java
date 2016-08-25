bee0cfff4ffeb0eb36ef30903fb941575443b371
zhlee-cf <1281861976@qq.com>
2016-08-25 16:49:55 +0800

    添加一键分享功能

--- openIM/src/main/java/com/open/im/activity/ClientActivity.java
+++ openIM/src/main/java/com/open/im/activity/ClientActivity.java
@@ -24,6 +24,8 @@ import java.io.File;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * 关于客户端界面
@@ -35,6 +37,8 @@ public class ClientActivity extends BaseActivity implements View.OnClickListener
     TextView tvVersion;
     @BindView(R.id.iv_qrcode)
     ImageView ivQrcode;
+    @BindView(R.id.tv_share)
+    TextView tvShare;
     private ClientActivity act;
     private String filePath;
 
@@ -131,7 +135,7 @@ public class ClientActivity extends BaseActivity implements View.OnClickListener
         super.onBackPressed();
     }
 
-    @OnClick({R.id.ib_back, R.id.tv_back, R.id.iv_qrcode})
+    @OnClick({R.id.ib_back, R.id.tv_back, R.id.iv_qrcode, R.id.tv_share})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_qrcode:
@@ -144,6 +148,39 @@ public class ClientActivity extends BaseActivity implements View.OnClickListener
 //                startActivity(intent);
                 finish();
                 break;
+            case R.id.tv_share:
+                showShare();
+                break;
         }
     }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("OpenIM下载地址:" + MyConstance.CLIENT_URL);
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
+
 }
